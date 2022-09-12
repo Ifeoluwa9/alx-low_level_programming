ad11637860def66ba7ad646c2072c782f8a0902f
Ifeoluwa9 <agnesolufemi@gmail.com>
2022-09-12 16:16:58 -0700

    If-else 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,7 +1,6 @@
 #include<stdio.h>
 
 /**
- *
  * main - print if the number is postive, zero, or negative
  *
  * Description: using the main function
@@ -17,7 +16,7 @@ int main(void)
 	{
 		putchar(c);
 	}
-	for (c ='a'; c <= 'f'; c++)
+	for (c = 'a'; c <= 'f'; c++)
 	{
 		putchar(c);
 	}
