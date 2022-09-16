2ea0284d5a275e763c399c98d395815242aaf706
Ifeoluwa9 <agnesolufemi@gmail.com>
2022-09-15 17:30:38 -0700

    debugging 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -4,7 +4,7 @@
 *largest_number - returns the largest of 3 numbers
 *@a: first integer
 *@b: second integer
-*c: third integer
+*@c: third integer
 *Return: Largest number
 */
 
@@ -14,35 +14,7 @@ if (a >= b && a >= c)
 {
 largest = a;
 }
-else if (b 
-.= a && b >= c)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+else if (b = a && b >= c)
 largest = b;
 }
 else
