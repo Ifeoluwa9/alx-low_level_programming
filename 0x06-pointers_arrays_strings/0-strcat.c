a1bec2a7cd5e0d7a2c4a1b85515f12865c2674e0
Ifeoluwa9 <agnesolufemi@gmail.com>
2022-09-22 21:21:24 -0700

    Tasks

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -15,12 +15,11 @@ char *_strcat(char *dest, char *src)
 	int index = 0, dest_len = 0;
 
 	while (dest[index++])
-			dest_len++;
+		dest_len++;
 
-			for (index = 0; src[index]; index++)
-			dest[dest_len++] = src[index];
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
 
-			return(dest);
-
+	return(dest);
 }
 			 
