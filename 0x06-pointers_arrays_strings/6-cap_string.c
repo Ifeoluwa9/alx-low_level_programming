92c3efcccd3c21513dab1cbff2d34708acc0c389
Ifeoluwa9 <agnesolufemi@gmail.com>
2022-09-22 21:36:47 -0700

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,25 +9,28 @@
 
 char *cap_string(char *s)
 {
-	int i = 0, j;
-	char a[] = " \t\n,;.!?\"() ();
+	int count;
 
-	while (*(s + i))
-	{
-		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+	/* scan through string */
+	count = 0;
+	while (s[count] != '\0')
+	{/* if next character after count is a char , capitalise it */
+		if (s[0] >= 97 && s[0] <= 122)
 		{
-			 if (i == 0)
-				 *(s + i) -= 'a' - 'A';
-			 else
-			 {
-				 for (j = 0; j <= 12; j++)
-				 {
-					 if (a[j] == (*s + i -1))
-						 *(s +i) -= 'a' -'A';
-				 }
-			 }
+			s[0] = s[0] - 32;
 		}
-		i++;
+		if (s[count] == ' ' || s[count] == '\t' || s[count] == '\n'
+				|| s[count] == ',' || s[count] == ';' || s[count] == '.'
+				|| s[count] == '.' || s[count] == '!' || s[count] == '?'
+				|| s[count] == '"' || s[count] == '(' || s[count] == ')'
+				|| s[count] == '{' || s[count] == '}')
+		{
+			if (s[count + 1] >= 97 && s[count + 1] <= 122)
+			{
+				s[count + 1] = s[count + 1] - 32;
+			}
+		}
+		count++;
 	}
 	return (s);
 }
