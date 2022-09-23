5854c01cfefb5fcb103463a75f283a54dd5bc66e
Ifeoluwa9 <agnesolufemi@gmail.com>
2022-09-22 20:52:36 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,7 +5,7 @@
  * inputted number of bytes
  * @dest: string to be appended upon@src: string to be completed at end of dest
  * @n: integer parameter to compare index to 
- * Return : retuns new concatenated sting
+ * Return : returns new concatenated sting
  */
 
 char *_strncat(char *dest, char *src, int n)
@@ -16,5 +16,6 @@ char *_strncat(char *dest, char *src, int n)
 		dest_len++;
 	for (index = 0; src[index] && index < n; index++)
 		dest[dest_len++] = src[index];
+
 	return (dest);
 }
