a81146eba8c4e86bfd3aa8a6a3f316e747327a15
Ifeoluwa9 <agnesolufemi@gmail.com>
2022-09-27 11:54:11 -0700

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,11 +12,11 @@
  * Return: returns pointer to first occurence of c
  */
 
-char*_strchr(char *s, char c)
+char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i =0; s[i] != c) && (s[i] != '\0'); i++)
+	for (i = 0; s[i] != c) && (s[i] != '\0'); i++)
 		;
 	if (s[i] == c)
 		return (s + i);
