ddb53ac9f37b700e810370acec04c57bc95c0ef4
Ifeoluwa9 <agnesolufemi@gmail.com>
2022-09-27 19:14:19 -0700

    Task 9

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -9,12 +9,12 @@
  */
 
 #include <stdio.h>
-#include <sttdef.h>
+#include <stddef.h>
 
 void print_chessboard(char (*a)[8]);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
-char *_strchr(char *s, char c);
+char *_stirchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
