8d798cefe3ecab47bf65fd09d96f0da5c5c6b85b
Ifeoluwa9 <agnesolufemi@gmail.com>
2022-10-03 09:08:18 -0700

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -3,8 +3,8 @@
 
 /**
  * main - program that prints its name, followed by a new line
- * @argc: argument that contains argument input
- * argv: argument that stores the strings in an array of char* (strings)
+ * @argc: argument that counts argument input
+ * @argv: argument that stores the strings in an array of char* (strings)
  * Return: 0
  */
 int main(int argc, char *argv[])
@@ -12,6 +12,7 @@ int main(int argc, char *argv[])
 	int num_1, num_2, mul;
 
 	if (arg != 3)
+
 		printf("Error\n");
 	else
 	{
@@ -20,5 +21,5 @@ int main(int argc, char *argv[])
 		mul = num_1 * num_2;
 		printf("%d\n", mul);
 	}
-	Return (0);
+	return (0);
 }
