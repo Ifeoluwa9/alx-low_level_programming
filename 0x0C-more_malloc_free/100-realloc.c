1c775cbdeaaee388c9815e613491a1fb20a95eba
Ifeoluwa9 <agnesolufemi@gmail.com>
2022-10-06 13:20:22 -0700

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -11,8 +11,8 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *p; 
-	unsigned int i, max = new/-size;
+char *p; 
+	unsigned int i, max = new_size;
 	char *oldp = ptr;
 
 	if (ptr == NULL)
