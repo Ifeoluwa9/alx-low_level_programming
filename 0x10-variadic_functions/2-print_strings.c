9330b1f24a017097906b41640d9787be19ba059c
Ifeoluwa9 <agnesolufemi@gmail.com>
2022-10-13 05:32:27 -0700

    print strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -5,12 +5,11 @@
 /**
  * print_strings - prints strings, followed by a new line
  * @separator: separator to print between the strings
- * @n
- * ; number of strings to print
+ * @n: number of strings to print
  *
  * Return: void
  */
-void print_strings(const char *separator, const unsigned int n, ...)
+void print_strings(const char *separator, const unsigned int n, ...);
 {
 	unsigned int i;
 	char *str;
